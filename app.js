f4409639c6899a2b8d069cad1afe83e2922c03a8
bagebear <bagebear@gmail.com>
2021-08-22 18:45:01 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const { mongo } = require('mongoose')
 const mongoose = require('mongoose')
 
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
 
@@ -17,8 +19,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.render('index')
 })
 
 app.listen(3000, () => {
