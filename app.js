97f59dcfe62e82629463937631e1a5b8336bdc27
bagebear <bagebear@gmail.com>
2021-08-22 22:41:17 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { mongo } = require('mongoose')
 const mongoose = require('mongoose')
 
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const Todo = require('./models/todo')
 
@@ -24,6 +25,8 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   // get all todo document
   Todo.find()
@@ -31,6 +34,27 @@ app.get('/', (req, res) => {
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
   })
+
+app.get('/todos/new', (req,res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name  
+  const todo = new Todo({ name })
+
+  return todo.save()
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
 
 app.listen(3000, () => {
   console.log('App is running on port 3000')
