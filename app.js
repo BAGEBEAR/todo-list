4eefcf0740b855357d07682fa667cc944bd66c62
bagebear <bagebear@gmail.com>
2021-08-28 17:41:10 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override')
 
 const routes = require('./routes')
 require('./config/mongoose')
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
@@ -17,6 +17,6 @@ app.use(methodOverride('_method'))
 
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('App is running on port 3000')
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`)
 })
